3ffe0d4abd5eb24e8b79f92dfd8ac2f107af363e
Diego Pagini <ext_atsistemas10@es.gestamp.com>
2024-01-27 19:22:44 +0100

    router config

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -97,9 +97,9 @@ class _LoginForm extends ConsumerWidget {
               child: CustomFilledButton(
                 text: 'Ingresar',
                 buttonColor: Colors.black,
-                onPressed: () {
-                  ref.read(loginFormProvider.notifier).onFormSubmit();
-                },
+                onPressed: loginForm.isPosting
+                    ? null // This is the way to disable the buttons
+                    : ref.read(loginFormProvider.notifier).onFormSubmit,
               )),
           const Spacer(flex: 2),
           Row(
