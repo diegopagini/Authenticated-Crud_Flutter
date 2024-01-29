095a96f8dc01f816570fe91dc001abd123f87b00
Diego Pagini <ext_atsistemas10@es.gestamp.com>
2024-01-29 16:18:57 +0100

    inputs

--- lib/features/shared/infrastructure/inputs/inputs.dart
+++ lib/features/shared/infrastructure/inputs/inputs.dart
@@ -1,4 +1,8 @@
 export 'email.dart';
 export 'full_name.dart';
 export 'password.dart';
+export 'price.dart';
 export 'repeated_password.dart';
+export 'slug.dart';
+export 'stock.dart';
+export 'title.dart';
---
+++ lib/features/shared/infrastructure/inputs/price.dart
@@ -0,0 +1,36 @@
+import 'package:formz/formz.dart';
+
+// Define input validation errors
+enum PriceError { empty, valid }
+
+// Extend FormzInput and provide the input type and error type.
+class Price extends FormzInput<double, PriceError> {
+  // Call super.pure to represent an unmodified form input.
+  const Price.pure() : super.pure(0.0);
+
+  // Call super.dirty to represent a modified form input.
+  const Price.dirty(super.value) : super.dirty();
+
+  String? get errorMessage {
+    if (isValid || isPure) return null;
+
+    if (displayError == PriceError.empty) return 'El campo es requerido';
+    if (displayError == PriceError.valid) {
+      return 'El precio no puede ser menor a 0';
+    }
+
+    return null;
+  }
+
+  // Override validator to handle validating a given input value.
+  @override
+  PriceError? validator(double value) {
+    if (value.toString().isEmpty || value.toString().trim().isEmpty) {
+      return PriceError.empty;
+    }
+
+    if (value <= 0) return PriceError.valid;
+
+    return null;
+  }
+}
